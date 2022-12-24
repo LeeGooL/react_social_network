e62968530f0fc36285ad6fe5bd66558510679f23
Egor <buruegoserg@gmail.com>
2022-12-24 11:32:05 +0300

    routing setup

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,14 @@
 import { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { LoginPage } from './pages/LoginPage';
+import { RegistrationPage } from './pages/RegistrationPage';
 
 export const App: FC = () => {
-  return <p>App</p>;
+  return (
+    <Routes>
+      <Route path="*" element={<span>Hello</span>} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="registration" element={<RegistrationPage />} />
+    </Routes>
+  );
 };
---
+++ front/src/pages/RegistrationPage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RegistrationPage = () => {
+	return (
+		<p>registration page</p>
+	)
+}
