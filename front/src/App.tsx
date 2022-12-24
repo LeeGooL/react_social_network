963d87b81df1d8b3c75594b484c15ee4bf889449
Egor <buruegoserg@gmail.com>
2022-12-24 12:36:08 +0300

    adding main bar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,17 @@
 import { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { MainBar } from './components/MainBar';
 import { LoginPage } from './pages/LoginPage';
 import { RegistrationPage } from './pages/RegistrationPage';
 
-export const App: FC = () => {
-  return (
+export const App: FC = () => (
+  <>
+    <MainBar />
+
     <Routes>
-      <Route path="*" element={<span>Hello</span>} />
+      <Route path="*" element={<p>Hello</p>} />
       <Route path="login" element={<LoginPage />} />
       <Route path="registration" element={<RegistrationPage />} />
     </Routes>
-  );
-};
+  </>
+);
