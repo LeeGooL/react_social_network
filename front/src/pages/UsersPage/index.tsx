9088b9f5cf62676e37cc767424c716f5e7c0a44f
Egor <buruegoserg@gmail.com>
2023-01-05 14:47:33 +0300

    error handling, loading and displaying users

--- front/src/pages/UsersPage/index.tsx
+++ front/src/pages/UsersPage/index.tsx
@@ -1,18 +1,22 @@
 import {
   Avatar,
+  Box,
   Card,
   CardContent,
   CardHeader,
+  CircularProgress,
   List,
   ListItem,
   ListItemAvatar,
   ListItemButton,
   ListItemText,
+  Typography,
 } from '@mui/material';
 import Badge from '@mui/material/Badge';
 import Container from '@mui/material/Container';
 import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
+import { useGetUsersQuery } from '../../redux/services/users';
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
   '& .MuiBadge-badge': {
@@ -21,8 +25,9 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
     boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
     '&::after': {
       position: 'absolute',
-      top: '10%',
-      left: '10%',
+      top: '11.5%',
+      left: '11.5%',
+      transform: 'translate(-2%, -2%)',
       width: '50%',
       height: '50%',
       borderRadius: '50%',
@@ -44,34 +49,62 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
 }));
 
 export const UsersPage = () => {
+  const { data: users, isLoading, isSuccess, error, isError } = useGetUsersQuery();
+
   return (
     <Container sx={{ p: 2 }}>
       <Card>
-        <CardHeader title="Пользователи:" />
+        <CardHeader title={isLoading || !isSuccess ? 'Загрузка...' : 'Пользователи:'} />
 
         <CardContent sx={{ p: 0 }}>
-          <List sx={{ p: 0 }} dense>
-            {[1, 2, 3].map((item) => (
-              <ListItem key={item} sx={{ p: 0 }}>
-                <ListItemButton component={Link} to="/users/1">
-                  <ListItemAvatar>
-                    <StyledBadge
-                      overlap="circular"
-                      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                      variant="dot"
-                    >
-                      <Avatar
-                        src="\sets\hair-style-street-fashion-beautiful-girl.jpg"
-                        variant="rounded"
-                        sx={{ width: '40px' }}
-                      />
-                    </StyledBadge>
-                  </ListItemAvatar>
-                  <ListItemText primary="Алексей Данчин" secondary="Мой статус" />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
+          {(isError || !users) && !isLoading && !isSuccess && (
+            <>
+              <Typography>Что-то пошло не так!</Typography>
+              <pre>
+                {error &&
+                  (typeof error === 'string'
+                    ? error
+                    : 'data' in error
+                    ? JSON.stringify(error.data)
+                    : 'message' in error
+                    ? error.message
+                    : 'Что-то не так!')}
+              </pre>
+            </>
+          )}
+
+          {isLoading || !isSuccess ? (
+            <Box justifyContent="center" display="flex">
+              <CircularProgress />
+            </Box>
+          ) : (
+            <List sx={{ p: 0 }} dense>
+              {users && users.length ? (
+                users.map(({ id, name, surname, status, avatar }) => (
+                  <ListItem key={id} sx={{ p: 0 }}>
+                    <ListItemButton component={Link} to={`/users/${id}`}>
+                      <ListItemAvatar>
+                        <StyledBadge
+                          overlap="circular"
+                          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                          variant="dot"
+                        >
+                          <Avatar src={avatar} variant="rounded" sx={{ width: '40px' }} />
+                        </StyledBadge>
+                      </ListItemAvatar>
+                      <ListItemText primary={`${name} ${surname}`} secondary={status} />
+                    </ListItemButton>
+                  </ListItem>
+                ))
+              ) : (
+                <Box justifyContent="center" display="flex">
+                  <Typography>
+                    Пользователей еще нет. Стань <Link to="/registration">первым</Link>!
+                  </Typography>
+                </Box>
+              )}
+            </List>
+          )}
         </CardContent>
       </Card>
     </Container>
